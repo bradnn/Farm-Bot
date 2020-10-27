db30987d383ffa711c902e771f056770e5b79a64
sycles <73144954+sycles@users.noreply.github.com>
2020-10-27 08:48:47 -0700

    Update bot.js

--- src/bot.js
+++ src/bot.js
@@ -2,13 +2,8 @@ const { FarmBot } = require('./structures/FarmBot.js');
 const Config = require('../config-test.js');
 
 const mongoose = require('mongoose');
-mongoose.connect(Config.mongoURI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-});
+mongoose.connect(Config.mongoURI, { useNewUrlParser: true, useUnifiedTopology: true });
 
-const client = new FarmBot(Config, {
-    disableEveryone: true
-});
+const client = new FarmBot(Config, { disableEveryone: true });
 
 client.connect();
